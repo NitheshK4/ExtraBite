ad5183e72ac538913e291b9f26a5d18a88f2b316
Jawahar Bharathi <cjawaharbharathi@gmail.com>
2026-08-11 21:35:38 +0530

    fix(android): configure installed ndkVersion 28.2.13676358 in app build.gradle.kts

--- ExtraBiteMobile/android/app/build.gradle.kts
+++ ExtraBiteMobile/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.extrabite.extrabite_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
